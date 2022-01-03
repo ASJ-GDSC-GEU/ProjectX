ca174fe65bf0a27da56d6c0aae5ae52d8893b615
Avneet <avneetmaankiya@gmail.com>
2022-01-04 03:40:11 +0530

    video meeting UI updated

---
+++ app/src/main/java/com/example/projectx/screens/LectureHall/MeetingHome.kt
@@ -0,0 +1,12 @@
+package com.example.projectx.screens.LectureHall
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectx.R
+
+class MeetingHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meeting_home)
+    }
+}
