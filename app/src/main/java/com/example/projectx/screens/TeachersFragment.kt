e99082d97100af2bb0e4b0a8f9453da76c494198
Avneet <avneetmaankiya@gmail.com>
2022-01-03 23:35:00 +0530

    video meeting

--- app/src/main/java/com/example/projectx/screens/TeachersFragment.kt
+++ app/src/main/java/com/example/projectx/screens/TeachersFragment.kt
@@ -5,12 +5,16 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.projectx.adapter.MyClassAdapter
 import com.example.projectx.R
+import com.example.projectx.adapter.MyClassAdapter
 import com.example.projectx.daos.MyClassDao
 import com.example.projectx.databinding.FragmentTeachersBinding
 import com.example.projectx.models.MyClass
@@ -42,6 +46,12 @@ class TeachersFragment : Fragment() {
     ): View? {
         _binding = FragmentTeachersBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.createMeet.setOnClickListener {
+            val action = TeachersFragmentDirections.actionTeachersFragmentToMeetingsActivity()
+            requireView().findNavController().navigate(action)
+        }
+
         return view
     }
 
